5b3dd4cc670ee19cf9e3a40cfdb431bdaf916060
StarrySnowy <kseun0349@gmail.com>
2024-10-03 15:06:34 +0200

    #2.3 Hello World_2
    #2.3 완성

--- toonflix_eun/lib/main.dart
+++ toonflix_eun/lib/main.dart
@@ -8,7 +8,14 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello world!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
     );
   }
 }
