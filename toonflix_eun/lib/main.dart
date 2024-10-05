9500f64f3a806992052699e5f57d9006893d2757
StarrySnowy <kseun0349@gmail.com>
2024-10-05 10:33:42 +0200

    #3.0 Header

--- toonflix_eun/lib/main.dart
+++ toonflix_eun/lib/main.dart
@@ -1,28 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Player {
-  String name;
-
-  Player({required this.name});
-}
-
 void main() {
-  var nico = Player(name: "potato");
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-        ),
-        body: Center(
-          child: Text('Hello world!'),
-        ),
-      ),
+          backgroundColor: Color(0xFF181818),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text('Hey, Selena',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800,
+                              )),
+                          Text('Welcome back',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 18,
+                              )),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ]),
+          )),
     );
   }
 }
